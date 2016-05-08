0b135d00b73aec04ec5497c98f69d6d475d82f73
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2016-05-08 05:18:40 +0200

    Refactor tools.js into a single object

--- tools.js
+++ tools.js
@@ -25,65 +25,62 @@ function callback (cb) {
   };
 }
 
-module.exports.codesign = function (identity, entitlement, file, cb) {
-  /* use the --no-strict to avoid the "resource envelope is obsolete" error */
-  const args = [ '--no-strict' ]; // http://stackoverflow.com/a/26204757
-  if (identity !== undefined) {
-    args.push('-fs', identity);
-  } else {
-    return cb('--identity is required to sign');
-  }
-  if (entitlement !== undefined) {
-    args.push('--entitlements=' + entitlement);
-  }
-  args.push(file);
-  execProgram(path.codesign, args, null, callback(cb));
-};
-
-module.exports.verifyCodesign = function (file, cb) {
-  const args = ['-v', '--no-strict', file];
-  execProgram(path.codesign, args, null, callback(cb));
-};
-
-module.exports.getEntitlementsFromMobileProvision = function (file, cb) {
-  const args = [ 'cms', '-D', '-i', file ];
-  execProgram(path.security, args, null, callback((error, stdout) => {
-    cb(error, plist.parse(stdout)['Entitlements']);
-  }));
-};
-
-module.exports.zip = function (cwd, ofile, src, cb) {
-  const args = [ '-qry', ofile, src ];
-  execProgram(path.zip, args, { cwd: cwd }, callback(cb));
-};
-
-module.exports.unzip = function (ifile, odir, cb) {
-  const args = [ '-o', ifile, '-d', odir ];
-  execProgram(path.unzip, args, {}, callback(cb));
-};
-
-module.exports.getIdentities = function (cb) {
-  const args = [ 'find-identity', '-v', '-p', 'codesigning' ];
-  execProgram(path.security, args, null, callback((error, stdout) => {
-    if (error) {
-      return cb(error);
+module.exports = {
+  codesign: function (identity, entitlement, file, cb) {
+    /* use the --no-strict to avoid the "resource envelope is obsolete" error */
+    const args = [ '--no-strict' ]; // http://stackoverflow.com/a/26204757
+    if (identity !== undefined) {
+      args.push('-fs', identity);
+    } else {
+      return cb('--identity is required to sign');
     }
-    const lines = stdout.split('\n');
-    lines.pop(); // remove last line
-    let ids = [];
-    for (let line of lines) {
-      const tok = line.indexOf(') ');
-      if (tok !== -1) {
-        const msg = line.substring(tok + 2).trim();
-        const tok2 = msg.indexOf(' ');
-        if (tok2 !== -1) {
-          ids.push({
-            'hash': msg.substring(0, tok2),
-            'name': msg.substring(tok2 + 1)
-          });
+    if (entitlement !== undefined) {
+      args.push('--entitlements=' + entitlement);
+    }
+    args.push(file);
+    execProgram(path.codesign, args, null, callback(cb));
+  },
+  verifyCodesign: function (file, cb) {
+    const args = ['-v', '--no-strict', file];
+    execProgram(path.codesign, args, null, callback(cb));
+  },
+  getEntitlementsFromMobileProvision: function (file, cb) {
+    const args = [ 'cms', '-D', '-i', file ];
+    execProgram(path.security, args, null, callback((error, stdout) => {
+      cb(error, plist.parse(stdout)['Entitlements']);
+    }));
+  },
+  zip: function (cwd, ofile, src, cb) {
+    const args = [ '-qry', ofile, src ];
+    execProgram(path.zip, args, { cwd: cwd }, callback(cb));
+  },
+  unzip: function (ifile, odir, cb) {
+    const args = [ '-o', ifile, '-d', odir ];
+    execProgram(path.unzip, args, {}, callback(cb));
+  },
+  getIdentities: function (cb) {
+    const args = [ 'find-identity', '-v', '-p', 'codesigning' ];
+    execProgram(path.security, args, null, callback((error, stdout) => {
+      if (error) {
+        return cb(error);
+      }
+      const lines = stdout.split('\n');
+      lines.pop(); // remove last line
+      let ids = [];
+      for (let line of lines) {
+        const tok = line.indexOf(') ');
+        if (tok !== -1) {
+          const msg = line.substring(tok + 2).trim();
+          const tok2 = msg.indexOf(' ');
+          if (tok2 !== -1) {
+            ids.push({
+              'hash': msg.substring(0, tok2),
+              'name': msg.substring(tok2 + 1)
+            });
+          }
         }
       }
-    }
-    cb(undefined, ids);
-  }));
+      cb(undefined, ids);
+    }));
+  }
 };
