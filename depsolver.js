nowsecure/node-applesign

commit c29d4bc7ef9e52cb133da36ac9ad4578eca12351

Do not sign and warn about suspicious binaries

session.js
@@ -626,10 +626,27 @@ module.exports = class ApplesignSession {
     return this;
   }
 
+  filterLibraries (libraries) {
+    return libraries.filter(_ => {
+      // we want to resign all frameworks. even if not referenced :?
+      if (_.indexOf('Frameworks/') !== -1) {
+        return true;
+      }
+      // check if there's a Plist to inform us which is the right executable
+      const plist = path.join(path.dirname(_), 'Info.plist');
+      const exe = getExecutable(path.dirname(_), path.basename(_));
+      if (path.basename(_) !== exe) {
+        console.error('Not signing', _);
+        return false;
+      }
+      return true;
+    });
+  }
+
   signLibraries (bpath, appdir, next) {
     this.emit('message', 'Signing libraries and frameworks');
 
-    const libraries = [];
+    let libraries = [];
     const exe = path.sep + getExecutable(this.config.appdir);
     const folders = this.config.appbin.split(path.sep);
     const exe2 = path.sep + folders[folders.length - 1];
@@ -749,6 +766,7 @@ module.exports = class ApplesignSession {
     };
 
     this.emit('message', 'Resolving signing order using layered list');
+    libraries = this.filterLibraries(libraries);
     depSolver(bpath, libraries, this.config.parallel, (err, libs) => {
       if (err) {
         return next(err);

commit 652cffb817d52e59517825f0066d7f36591d30fa

Add try/catch around readFileSync for filterLibraries
Fixes regression introduced in c29d4bc7ef9e52cb133da36ac9ad4578eca12351

depsolver.js
@@ -87,7 +87,7 @@ function getMachoLibs (file, cb) {
         }
       }
       if (exec === null) {
-        /* skip fat file here*/
+        /* skip fat file here */
         return cb(null, []);
       }
       const libs = exec.cmds.filter((x) => {
@@ -119,7 +119,7 @@ function layerize (state) {
       }
       let allDepsSolved = true;
       for (let dep of deps) {
-        const depLayer = state[dep]? state[dep].layer: 0;
+        const depLayer = state[dep] ? state[dep].layer : 0;
         if (depLayer === -1 || depLayer === currentLayer) {
           allDepsSolved = false;
           break;

example.js
@@ -29,4 +29,3 @@ function onEnd (error) {
     console.log('ios-deploy -b', s.config.outfile);
   }
 }
-

index.js
@@ -57,7 +57,7 @@ module.exports = class Applesign {
       s.setFile(file);
     }
     return s.signIPA((err) => {
-      s.finalize(cb, err)
+      s.finalize(cb, err);
     });
   }
 

session.js
@@ -102,10 +102,14 @@ function parentDirectory (root) {
 function getExecutable (appdir, exename) {
   if (appdir) {
     const plistPath = path.join(appdir, 'Info.plist');
-    const plistData = plist.readFileSync(plistPath);
-    const cfBundleExecutable = plistData['CFBundleExecutable'];
-    if (cfBundleExecutable) {
-      return cfBundleExecutable;
+    try {
+      const plistData = plist.readFileSync(plistPath);
+      const cfBundleExecutable = plistData['CFBundleExecutable'];
+      if (cfBundleExecutable) {
+        return cfBundleExecutable;
+      }
+    } catch (e) {
+  // do nothing
     }
   }
   return exename;
@@ -151,9 +155,9 @@ module.exports = class ApplesignSession {
 
   finalize (cb, error) {
     if (error && !this.config.noclean) {
-      return this.mrproper (_ => { cb(error); });
+      return this.mrproper(_ => { cb(error); });
     }
-    return cb (error);
+    return cb(error);
   }
 
   /* Public API */
@@ -538,7 +542,7 @@ module.exports = class ApplesignSession {
         } else {
           this.emit('message', 'Unknown device type: ' + type);
         }
-      }
+      };
       const oldSupportedDevices = data['UISupportedDevices'];
       if (oldSupportedDevices) {
         this.emit('message', 'Empty UISupportedDevices');

tools.js
@@ -126,7 +126,7 @@ function getMobileProvisionPlist (file, cb) {
 
 function getEntitlementsFromMobileProvision (file, cb) {
   return getMobileProvisionPlist(file, (e, o) => {
-    return e? cb(e, o): cb(e, o['Entitlements']);
+    return e ? cb(e, o) : cb(e, o['Entitlements']);
   });
 }
 