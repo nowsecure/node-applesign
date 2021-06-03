cde5a2011c1b3e97c5746398463ff338065e8a6b
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-06-03 02:17:24 +0200

    Fix rebundling apps with app extensions (.share only for now)

--- index.js
+++ index.js
@@ -114,6 +114,20 @@ class Applesign {
     }
   }
 
+  async adjustAllInfoPlists () {
+    const infoPlistPath = path.join(this.config.appdir, 'Info.plist');
+    adjustInfoPlist(infoPlistPath, this.config, this.emit.bind(this));
+    const ls = new AppDirectory();
+    const res = await ls.loadFromDirectory(this.config.appdir);
+    for (let appex of ls.appexs) {
+      const lidx = appex.lastIndexOf('/');
+      if (lidx !== -1) {
+        const plistPath = path.join (appex.substring(0,lidx), 'Info.plist');
+        adjustInfoPlist(plistPath, this.config, this.emit.bind(this));
+      }
+    }
+  }
+
   async signAppDirectory (ipadir, skipNested) {
     fchk(arguments, ['string', 'boolean']);
     this._pullMobileProvision();
@@ -143,8 +157,7 @@ class Applesign {
     if (this.config.insertLibrary !== undefined) {
       await insertLibrary(this.config);
     }
-    const infoPlistPath = path.join(this.config.appdir, 'Info.plist');
-    adjustInfoPlist(infoPlistPath, this.config, this.emit.bind(this));
+    await this.adjustAllInfoPlists ();
     if (!this.config.pseudoSign) {
       if (!this.config.mobileprovision) {
         throw new Error('warning: No mobile provisioning file provided');
